e1e33e58f72ad336961c9a841d3620d7a4fb80ea
Karina <karinadesyatnik27@gmail.com>
2023-05-08 17:10:29 +0300

    all site

---
+++ docs/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+$('.menu-btn').click(function(){
+    $('.menu').addClass('active');
+});
+$('.close-btn').click(function(){
+    $('.menu').removeClass('active');
+})
+
+
+
+});$(".smooth").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 100) {
+      $(".top").fadeOut();
+    } else {
+      $(".top").fadeIn();
+    }
+  });
